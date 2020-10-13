1cd1d96f13dd4e089b504c6854af33d1e9da8c03
Solomon Pierce <62899156+spierceVR@users.noreply.github.com>
2020-10-12 20:14:33 -0500

    Delete nox.lua
    part of an unrelated playermodel addon

--- lua/autorun/nox.lua
+++
@@ -1,2 +0,0 @@
-player_manager.AddValidModel( "Nox", "models/playermodel/nox/nsquad/nsquadplayermodel.mdl" );
-list.Set( "PlayerOptionsModel", "Nox", "models/playermodel/nox/nsquad/nsquadplayermodel.mdl" );
